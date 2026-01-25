9a0cbbef2f7d4cf6f6e00dedd9b15c624d8843c7
tmadrinich <tmadrinich@gmail.com>
2026-01-25 10:41:08 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+// Load header
+fetch('partials/header.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('site-header').innerHTML = html;
+    initNav();
+    setActiveNav();
+  });
+
+// Load footer
+fetch('partials/footer.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('site-footer').innerHTML = html;
+  });
+
+function setActiveNav() {
+  const page = location.pathname.split('/').pop() || 'index.html';
+  const pageName = page.replace('.html', '');
+
+  document.querySelectorAll('.main-nav a').forEach(link => {
+    if (link.dataset.page === pageName) {
+      link.classList.add('active');
+    }
+  });
+}
 
