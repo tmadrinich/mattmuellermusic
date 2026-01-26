b7fb7f0a3f52eb88e01128e94c189369a075eeaf
tmadrinich <tmadrinich@gmail.com>
2026-01-25 18:03:33 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -88,3 +88,23 @@ function initLinkFade() {
     });
   });
 }
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextBtn = document.querySelector('.carousel-btn.next');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+
+let index = 0;
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % slides.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
